56ca2fa205910efc2f3d0f07aef77fd6c67dd87c
ginko lee <492180240@qq.com>
2024-11-28 16:13:07 +0800

    Initial

---
+++ src/main/java/com/dailyhealth/springhealthsystem/SpringHealthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dailyhealth.springhealthsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHealthSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHealthSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailyhealth/springhealthsystem/SpringHealthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyhealth.springhealthsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHealthSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
