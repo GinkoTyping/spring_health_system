0d79f5f0904c343474ba96eb6ad257e1e4430be0
ginko lee <492180240@qq.com>
2024-11-30 22:09:48 +0800

    更新sql脚本

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,15 @@
--- 插入一些示例用户（可选）
 INSERT INTO users (username, email, password)
 VALUES ('1', '1-email', '1'),
        ('1', '1-email', '1');
 
--- 插入一些示例数据种类（可选）
 INSERT INTO health_metrics_types (name, description, unit, data_type)
-VALUES ('Steps', 'Number of steps taken', 'steps', 'INTEGER'),
-       ('Heart Rate', 'Heart rate in beats per minute', 'bpm', 'FLOAT'),
-       ('Blood Pressure', 'Blood pressure reading in mmHg', 'mmHg', 'STRING');
--- 假设血压以字符串形式存储，如"120/80"
+VALUES ('步数', '总计步数', 'steps', 'INTEGER'),
+       ('心率', '每分钟心率', 'bpm', 'FLOAT'),
+       ('血压', '平均血压', 'mmHg', 'FLOAT');
+
+INSERT INTO health_metrics (user_id, metric_type_id, metric_type_name, value)
+VALUES (1, 1, '步数',3000);
+INSERT INTO health_metrics (user_id, metric_type_id, metric_type_name, value)
+VALUES (1, 2, '心率',70.0);
+INSERT INTO health_metrics (user_id, metric_type_id, metric_type_name, value)
+VALUES (1, 3, '血压',100.0);
